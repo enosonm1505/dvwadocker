b815e34b933dfef7d09a75a910d74e8b51bc3cd8
enosonm1505 <91190634+enosonm1505@users.noreply.github.com>
2022-08-08 10:10:08 +0530

    Create snyk.py

---
+++ snyk.py
@@ -0,0 +1,24 @@
+import requests
+
+headers = {
+    'accept': 'application/json',
+    # requests won't add a boundary if this header is set when you pass files=
+    # 'Content-Type': 'multipart/form-data',
+    'Authorization': 'Token da8af64aa09849f011a977d1950d230cb7b23963',
+}
+
+files = {
+    'minimum_severity': (None, 'Low'),
+    'active': (None, 'true'),
+    'verified': (None, 'true'),
+    'scan_type': (None, 'Trivy Scan'),
+    'close_old_findings': (None, 'true'),
+    'push_to_jira': (None, 'false'),
+    'file': open('snyk.json', 'rb'),
+    'product_name': (None, 'Testphp vulnweb'),
+    'scan_date': (None, '2022-06-14'),
+    'engagement_name': (None, 'Trivy'),
+}
+
+response = requests.post('http://161.97.136.120:8080/api/v2/import-scan/', headers=headers, files=files)
+print(response.content)
